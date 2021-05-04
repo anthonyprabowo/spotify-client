03c02968305a8f40e5c0091e4ca1a0c3cd85cf44
Anthony Prabowo <anthonyprabowo@users.noreply.github.com>
2021-05-04 00:30:39 -0700

    added loading button while the content is loading

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -16,6 +16,7 @@ export default function MainPage({code}) {
   const [searchResult, setSearchResult] = useState([]);
   const [currentSong, setCurrentSong] = useState();
   const [lyrics, setLyrics] = useState("");
+  const [loading, setLoading] = useState(true);
 
   function chooseTrack(track) {
     setCurrentSong(track);
@@ -26,6 +27,7 @@ export default function MainPage({code}) {
   useEffect(() => {
     if(!accessToken) return;
     spotifyApp.setAccessToken(accessToken);
+    setLoading(false);
   }, [accessToken])
 
   useEffect(() => {
@@ -75,18 +77,27 @@ export default function MainPage({code}) {
       <div className="input-group rounded">
         <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
           aria-describedby="search-addon" value={search} onChange={e => setSearch(e.target.value)}/>
-      </div>
-      <div className="flex-grow-1 my-2" style={{overflowY: "auto"}}>
-        {searchResult.map(track => (
-          <TrackResult track={track} chooseTrack={chooseTrack} key={track.uri} />
-        ))}
+      </div> 
 
-        {searchResult.length === 0 && 
-          <div className="text-center" style={{whiteSpace: "pre"}}>
-          {lyrics}
-          </div>
-        }
-      </div>
+      {loading ? 
+        <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "100vh"}}>
+          <p style={{ fontSize: "2em" }}><i class="fa fa-spinner fa-spin"></i>  Loading</p>
+        </div> : 
+        <div className="flex-grow-1 my-2" style={{overflowY: "auto"}}>
+          {searchResult.map(track => (
+            <TrackResult track={track} chooseTrack={chooseTrack} key={track.uri} />
+          ))}
+
+          {searchResult.length === 0 && 
+            <div className="text-center" style={{whiteSpace: "pre"}}>
+            {lyrics}
+            </div>
+          }
+        </div> 
+      }
+      
+      
+      
       <div>
         <Player accessToken={accessToken} trackUri={currentSong?.uri} />
       </div>
